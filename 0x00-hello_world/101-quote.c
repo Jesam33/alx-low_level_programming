f178a2fe46582596b43d85352e9ed9af3ff6d04c
jesam33 <obasesamgospel@gmail.com>
2023-07-18 09:18:55 -0700

    tasks

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - my entry pont
+ * Return: always return 1 (fail)
+ */
+int main(void)
+{
+	fputs("and that piece of art is useful\" - Dora Korpar, ", stdout);
+	fputs("2015-10-19\n", stdout);
+	return (1);
+}
